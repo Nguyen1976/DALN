6b474ffcc212909c70e90ef2269047a93aad81ca
Nguyen1976 <nguyen2202794@gmail.com>
2026-06-19 14:32:28 +0700

    update(frontend): update api root for frontend

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -1,2 +1,2 @@
-export const API_ROOT = "ec2-13-214-162-28.ap-southeast-1.compute.amazonaws.com";
+export const API_ROOT = "ec2-13-214-162-28.ap-southeast-1.compute.amazonaws.com:8000";
 // export const API_ROOT = "http://localhost:8000";
